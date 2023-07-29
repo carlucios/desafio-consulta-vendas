0fce582f67e04eb859e23330c69197614ca1285a
carlucios <carlucios@gmail.com>
2023-07-29 19:14:19 -0300

    Adicionando as funcionalidades referente ao getMapping /report

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleWithSellerNameDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleWithSellerNameDTO(obj.id, obj.amount, obj.date, obj.seller.name) " +
+            "FROM Sale obj " +
+            "WHERE UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))" +
+            "AND obj.date BETWEEN :minDate AND :maxDate")
+    List<SaleWithSellerNameDTO> searchSaleWithFilters(LocalDate minDate, LocalDate maxDate, String name);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,7 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleWithSellerNameDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +27,24 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleWithSellerNameDTO> searchSaleWithFilters(String minDateStr, String maxDateStr, String name) {
+		LocalDate minDate;
+		LocalDate maxDate;
+		if (Objects.equals(maxDateStr, "")) {
+			LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+			maxDate = today;
+		} else {
+			maxDate = LocalDate.parse(maxDateStr);
+		}
+		if (Objects.equals(minDateStr, "")) {
+			minDate = maxDate.minusYears(1L);
+		} else {
+			minDate = LocalDate.parse(minDateStr);
+		}
+
+		return repository.searchSaleWithFilters(minDate, maxDate, name);
 	}
 }
